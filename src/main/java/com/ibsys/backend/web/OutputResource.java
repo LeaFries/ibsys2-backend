3e24e73603f986c5d77afe9bad50addbd3ad1930
Taner Memili <Taner.Memili@web.de>
2023-04-29 09:31:23 +0200

    Added Swagger endpoint description for test ressource

--- src/main/java/com/ibsys/backend/web/OutputResource.java
+++ src/main/java/com/ibsys/backend/web/OutputResource.java
@@ -2,6 +2,7 @@ package com.ibsys.backend.web;
 
 import com.ibsys.backend.core.domain.aggregate.Output;
 import com.ibsys.backend.core.service.OutputService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/input")
+@RequestMapping("/api/output")
 @RequiredArgsConstructor
 public class OutputResource {
     private final OutputService outputService;
 
+    @Operation(summary = "Provides the final result of the production plan")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Output> findInput() {
         return ResponseEntity.ok(outputService.findInput());
