cde6d0a90c951021a42d06326692147bf53756d5
AlexanderStecknitz <Alexander@Stecknitz.eu>
2023-06-08 12:47:35 +0200

    IB-6: feat: just for commit

--- src/main/java/com/ibsys/backend/core/service/DispositionEigenfertigungService.java
+++ src/main/java/com/ibsys/backend/core/service/DispositionEigenfertigungService.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -47,7 +48,7 @@ public class DispositionEigenfertigungService {
     @Transactional
     public void dispositionEigenfertigungStart(final DispositionEigenfertigungInputDTO inputDTO) {
         updateArticles(inputDTO.getGeplanterSicherheitsbestand());
-        dispositionEigenfertigung();
+        dispositionEigenfertigung(Collections.emptyList());
     }
 
     @Transactional
